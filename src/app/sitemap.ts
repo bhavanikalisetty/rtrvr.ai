25b870dd36ecb1ec424514993c303aca1162303a
Bhavani Kalisetty <bkalisetty@google.com>
2024-06-15 21:42:37 -0700

    adding sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+        url: 'https://www.rtrvr.ai',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1
+    },
+    {
+        url: 'https://www.rtrvr.ai/build-datasets',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8
+    },
+    {
+        url: 'https://www.rtrvr.ai/explore-data-marketplace',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.6
+    },
+    {
+        url: 'https://www.rtrvr.ai/about',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5
+    }
+  ]
+}
